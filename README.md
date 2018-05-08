d194e66b64dbb5c59df7c727decae5c01321eb72
moore54 <moorekevin711@gmail.com>
2018-05-08 11:39:05 -0600

    Updated readme, made module, and setup tests

--- README.md
+++ README.md
@@ -12,7 +12,8 @@
 
 Turbine acoustic code using the BPM equations developed by Brooks, Pope, and Marcolini
 
-Developed by Eric Tingey at Brigham Young University, 2015-2017
+Developed by Eric Tingey at Brigham Young University, 2015-2017,
+Translated to Julia by Kevin Moore and Taylor McDonald at BYU, 2018
 
 This code models the acoustic propagation of a wind turbine based on turbulent boundary layer edge noise, separation stall noise, tip vortex formation noise, laminar boundary layer vortex shedding noise, and trailing edge bluntness vortex shedding noise. Turbulent inflow noise is not assumed in this current code. The semi-empirical equations were developed from the NACA 0012 airfoil data and the blade segments used in the test file are based on the NREL 5-MW wind turbine. Scaling of the segments is based on the blade length specified.
 
@@ -25,34 +26,18 @@ Vargas, L., "Wind Turbine Noise Prediction," Master's Thesis, Technical Universi
 
 ## Installation instructions
 
-- system requirements: gfortran (using MinGW for Windows in order to use the commands here), python 2.7, numpy, scipy, matplotlib
-- run:
-```
-python setup.py install
-```
-- or navigate to the directory and run the following command in the terminal to build the Fortran code:
+Pkg.clone("url-to-package-on-git")
 
-Mac
-```
-$ cd wake_model
-$ f2py -c  --opt=-O2 -m _bpmacoustic BPM_Acoustic_Model.f90
-$ f2py -c  --opt=-O2 -m _bpmvawtacoustic BPM_VAWT_Acoustic_Model.f90
-```
+## Running the Julia code
 
-Windows
-```
-cd wake_model
-python <\your\path\to\f2py.py> -c --opt=-O2 --compiler=mingw32 --fcompiler=gfortran -m _bpmacoustic BPM_Acoustic_Model.f90
-python <\your\path\to\f2py.py> -c --opt=-O2 --compiler=mingw32 --fcompiler=gfortran -m _bpmvawtacoustic BPM_VAWT_Acoustic_Model.f90
-```
-(<\your\path\to\f2py.py>: most likely C:\Python27\Scripts\f2py.py)
+This julia code can be run from another file using:
+```julia
 
-## Running the Python code
+import BPM
 
-This python code can be run from another file using:
-```python
-SPL_HAWT = _bpmacoustic.turbinepos(turbx, turby, obs, winddir, windvel, rpm, B, h, rad,\
+SPL_HAWT = BPM.turbinepos(turbx, turby, obs, winddir, windvel, rpm, B, h, rad,\
 c, c1, alpha, nu, c0, psi, AR, noise_corr)
+```
 """
 Calculating the sound pressure level for a HAWT
 
@@ -99,59 +84,8 @@ SPL_HAWT : float
     sound pressure level calculated at observer location (dB)
 """
 
-SPL_VAWT = _bpmvawtacoustic.turbinepos(ntheta, turbx, turby, obs, winddir, B, Hub,\
-high, rad, c, c1, alpha, nu, c0, psi, AR, noise_corr, rot, Vinf, wakex, wakey)
-"""
-Calculating the sound pressure level for a VAWT
+## Run tests
 
-Parameters
-----------
-ntheta : int
-    number of points along blade flight path to calculate velocities
-turbx : array
-    x-positions of all the turbines heard by an observer (east to west, meter)
-turby : array
-    y-positions of all the turbines heard by an observer (north to south, meter)
-obs : array
-    x-, y-, and z-position of a specified observer (E-W, N-S, height; meter)
-winddir : float
-    direction the wind blows from (180=N, 270=E, 0=S, 90=W; degree)
-B : float
-    number of blades on a turbine
-Hub : float
-    hub height of a turbine (meter)
-high : array
-    height positions along the turbine blade (meter)
-rad : float
-    turbine radius (meter)
-c : array
-    chord length at each radial segment (meter)
-c1 : array
-    distance from the pitch axis to leading edge at each radial segment (meter)
-alpha : array
-    angle of attack of each radial segment (degree)
-nu : float
-    kinematic viscosity of the air (m^2/s)
-c0 : float
-    speed of sound of the air (m/s)
-psi : float
-    solid angle of turbine blades between upper and lower sides of trailing edge (degree)
-AR : float
-    aspect ratio of turbine blades
-noise_corr : float
-    correction factor for SPL calculations (1=none, use if calculations differ from expected)
-rot : array
-    rotation rate of each turbine (rad/s)
-Vinf : float
-    free stream wind speed (m/s)
-wakex : array
-    the wake influenced x-velcoity of the turbine at each point along the blade flight path (m/s)
-wakey : array
-    the wake influenced y-velcoity of the turbine at each point along the blade flight path (m/s)
-
-Returns
-----------
-SPL_VAWT : float
-    sound pressure level calculated at observer location (dB)
-"""
+```julia
+Pkg.test("BPM")
 ```
--- src/BPM.jl
+++ src/BPM.jl
@@ -1,6 +1,10 @@
-# module BPM
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Eric Tingey, Brigham Young University
+#
+# Modified 2018 Kevin Moore, Taylor McDonald BYU
 
-
+module BPM
 # Subroutine to use the BPM equations for turbine acoustics
 
 # cubic spline interpolation setup (for Tip Vortex Noise)
@@ -882,65 +886,4 @@ function turbinepos(x,y,obs,winddir,windvel,rpm,B,Hub,
 end #turbinepos
 
 
-# end # module
-
-
-##################################################################################
-##################################################################################
-##################################################################################
-
-# Rosiere Valdiation (243.84 m, 800 ft should be 47 dB)
-# http://www.mge.com/environment/green-power/wind/rosiere.htm
-x_test = [0.] # x-locations of turbines (m)
-y_test = [0.] # y-locations of turbines (m)
-obs_test = [0., 243.84, 0.] # x-, y-, and z-location of the observer (m)
-winddir_test = 0. # wind direction (deg)
-rpm_test = [28.5] # rotation rate of the tubrines (rpm)
-windvel_test = [15.] # wind velocity (m/s)
-B_test = 3 # number of blades
-h_test = 25. # height of the turbine hub (m)
-noise_corr = 0.8697933840957954 # correction factor for noise
-
-rad =  [1.069324603174603, 2.088888888888889, 3.1084531746031745, 4.382936507936508, 5.912301587301587, 7.441666666666666, 8.971031746031747, 10.500396825396825, 12.029761904761905, 13.559126984126985, 15.088492063492065, 16.617857142857144, 18.147222222222222, 19.6765873015873, 20.951070634920637, 21.97063492063492, 22.990199206349207, 23.5] # radial positions (m)
-c =  [2.8941253867777776, 3.1490568155396828, 3.404805332214286, 3.7234696181666673, 3.8010929698730163, 3.6425779148095243, 3.4718065410555554, 3.2740712661825397, 3.062445496793651, 2.861441870269841, 2.660438243746032, 2.459434617222222, 2.258430990698413, 2.057427364174603, 1.889924342071429, 1.7044453858888888, 1.1594477481190477] # chord lengths (m)
-alpha =  [13.308, 13.308, 13.308, 13.308, 11.48, 10.162, 9.011, 7.795, 6.544, 5.361, 4.188, 3.125, 2.319, 1.526, 0.863, 0.37, 0.106] # angles of attack (deg)
-c1 = c*0.25 # pitch axis (m)
-AR = 17. # blade aspect ratio
-
-nu = 1.78e-5 # kinematic viscosity (m^2/s)
-c0 = 343.2 # speed of sound (m/s)
-psi = 14.0 # solid angle (deg)
-                    # f    (nturb,  nseg,     nobs,       x,            y,          obs,    winddir,windvel,rpm, B, Hub, rad,  c,  c1,alpha,nu,    c0,      psi, AR, noise_corr)
-db_test_ros = turbinepos(x_test, y_test, obs_test, winddir_test, windvel_test, rpm_test, B_test, h_test, rad, c, c1, alpha, nu, c0, psi, AR, noise_corr)
-
-println("Test Cases:")
-println("Rosiere Validation (47 dB): $db_test_ros")
-
-##################################################################################
-##################################################################################
-##################################################################################
-
-# SPL Test Paremeters (changed to whatever desired)
-x_test = [0.,5.] # x-locations of turbines (m)
-y_test = [0.,5.] # y-locations of turbines (m)
-obs_test = [0., 200., 0.] # x-, y-, and z-location of the observer (m)
-winddir_test = 0. # wind direction (deg)
-rpm_test = [28.5,28.5] # rotation rate of the tubrines (rpm)
-windvel_test = [10.0,10.0] # wind velocity (m/s)
-B_test = 3 # number of blades
-h_test = 25. # height of the turbine hub (m)
-noise_corr = 0.8697933840957954 # correction factor for noise
-
-rad =  [1.069324603174603, 2.088888888888889, 3.1084531746031745, 4.382936507936508, 5.912301587301587, 7.441666666666666, 8.971031746031747, 10.500396825396825, 12.029761904761905, 13.559126984126985, 15.088492063492065, 16.617857142857144, 18.147222222222222, 19.6765873015873, 20.951070634920637, 21.97063492063492, 22.990199206349207, 23.5] # radial positions (m)
-c =  [2.8941253867777776, 3.1490568155396828, 3.404805332214286, 3.7234696181666673, 3.8010929698730163, 3.6425779148095243, 3.4718065410555554, 3.2740712661825397, 3.062445496793651, 2.861441870269841, 2.660438243746032, 2.459434617222222, 2.258430990698413, 2.057427364174603, 1.889924342071429, 1.7044453858888888, 1.1594477481190477] # chord lengths (m)
-alpha =  [13.308, 13.308, 13.308, 13.308, 11.48, 10.162, 9.011, 7.795, 6.544, 5.361, 4.188, 3.125, 2.319, 1.526, 0.863, 0.37, 0.106] # angles of attack (deg)
-c1 = c*0.25 # pitch axis (m)
-AR = 17. # blade aspect ratio
-
-nu = 1.78e-5 # kinematic viscosity (m^2/s)
-c0 = 343.2 # speed of sound (m/s)
-psi = 14.0 # solid angle (deg)
-
-db_test = turbinepos(x_test, y_test, obs_test, winddir_test, windvel_test, rpm_test, B_test, h_test, rad, c, c1, alpha, nu, c0, psi, AR, noise_corr)
-
-println("Test SPL: $db_test")
+end # module
